98dd141d6b69c74dde28882084781fbb6ea9729c
Christian Cozma <christiancozma.work@gmail.com>
2021-04-27 18:54:28 -0500

    add some css to demo user page

--- auth-me-project/frontend/src/components/UserProfile/Photo.js
+++ auth-me-project/frontend/src/components/UserProfile/Photo.js
@@ -1,16 +1,25 @@
-
+import "./photo.css"
 
 function Photo({image}) {
 console.log(image);
     return (
-        <div>
-            <h1></h1>
-            <img
-            className="images"
-            src={image.photoUrl}
-            alt="Spaaaaaaaace"
-            />
-        </div>
+        <>
+            <span className="master-span">
+                <h1></h1>
+                <div className="image-container">
+                            <h3>{image.title}</h3>
+                            <img
+                            className="images-space"
+                            src={image.photoUrl}
+                            alt="Spaaaaaaaace"
+                            />
+                            <h3>{image.authorCredited}</h3>
+                        <div id="img-desc">
+                            <p>{image.description}</p>
+                        </div>
+                </div>
+            </span>
+        </>
     )
 }
 export default Photo;
