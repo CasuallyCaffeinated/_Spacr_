6564dff378d56a26b109832b9923946fa3e2fa95
Christian Cozma <christiancozma.work@gmail.com>
2021-04-27 21:27:08 -0500

    add style to demo user profile pictures

--- auth-me-project/frontend/src/components/UserProfile/Photo.js
+++ auth-me-project/frontend/src/components/UserProfile/Photo.js
@@ -5,13 +5,13 @@ console.log(image);
     return (
         <>
                 <div className="image-container">
-                            <h3>{image.title}</h3>
+                            <h3 className="heading title">{image.title}</h3>
                             <img
                             className="images-space"
                             src={image.photoUrl}
                             alt="Spaaaaaaaace"
                             />
-                            <h3>{image.authorCredited}</h3>
+                            <h3 className="heading author">{image.authorCredited}</h3>
                         <div id="img-desc">
                             A description:
                             <p>{image.description}</p>
