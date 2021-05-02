cab362a8fc20f1f8fb13e9d3ba7a5cd735b8bf02
Christian Cozma <christiancozma.work@gmail.com>
2021-05-02 12:10:51 -0500

    added styles to logout dropdown

--- auth-me-project/frontend/src/components/Navigation/ProfileButton.js
+++ auth-me-project/frontend/src/components/Navigation/ProfileButton.js
@@ -39,8 +39,8 @@ function ProfileButton({ user }) {
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
+          {/* <li>{user.username}</li>
+          <li>{user.email}</li> */}
           <li>
             <button onClick={logout}>Log Out</button>
           </li>
